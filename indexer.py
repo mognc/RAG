8df8607574c00e882970cc86dd20a86675b6de1c
mognc <mogncgamer@gmail.com>
2024-09-24 11:26:02 +0500

    removed error

--- indexer.py
+++ indexer.py
@@ -2,7 +2,7 @@ import numpy as np
 import faiss
 
 # Load your index (adjust according to your setup)
-index = faiss.read_index('data/index.faiss')
+index = faiss.read_index('data/faiss_index_HNSWFLAT.index')
 
 def search_passages(query_embedding, index, k=5):
     D, I = index.search(query_embedding, k)  # Search for k nearest neighbors
